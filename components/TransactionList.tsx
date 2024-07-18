205efe2fbd27b88fa9f43c00607b246e93b41546
PieMenard <pinedo.m.gustavo@gmail.com>
2024-07-18 00:31:07 -0400

    add get transactions action

--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -1,12 +1,19 @@
+import getTransactions from '@/app/actions/getTransactions';
 import { Transaction } from '@/types/Transaction';
 
-const TransactionList = () => {
+const TransactionList = async () => {
+  const { transactions, error } = await getTransactions();
+
+  if (error) {
+    return <p className="error">{error}</p>;
+  }
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
         {transactions &&
-          TransactionList.map((transaction: Transaction) => (
+          transactions.map((transaction: Transaction) => (
             <p>{transaction.text}</p>
           ))}
       </ul>
